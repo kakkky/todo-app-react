c23ffcdfac4d1050d14d81518555b2d8ab5a5008
YutaKakiki <yutakakiki@kakinokiyuunoMacBook-Air.local>
2025-02-19 13:27:35 +0900

    ファイル構成を仮決め

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import "@/App.css";
-import { Button } from "./components/ui/button";
-
 function App() {
 	return (
 		<>
-			<div className="text-2xl">Bidfome</div>
-			<Button>ボタン</Button>
+			<div>Bidfome</div>
 		</>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@/styles/index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Biomeでは、non-nullアサーション（！）が禁止されている
+// よって、元の createRoot(root!).render{...}という記述を変更した
+const root = document.getElementById("root");
+if (root) {
+	createRoot(root).render(
+		<StrictMode>
+			<App />
+		</StrictMode>,
+	);
+}
